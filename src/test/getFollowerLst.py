95bba4837bb8137a89e5e224fe63c37d2b9523f1
Muffeter <sonmu.def@gmail.com>
2023-07-21 14:22:39 +0800

    1

---
+++ src/test/getFollowerLst.py
@@ -0,0 +1,10 @@
+import sys, os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+api_path = os.path.join(current_dir, '..', 'api')
+sys.path.append(api_path)
+from bot import Bot
+bot = Bot()
+lst = bot.getMedia("love", amount=10)
+user_pklst = (bot.getUser(l) for l in lst if Bot.getFollower_count(bot.getUser(l)) > 10000)
+for i in user_pklst:
+    print(i)
